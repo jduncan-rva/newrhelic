c7bc3c8e26c6ad0e400a1a477f5851ba597a393b
Jamie Duncan <jamie.e.duncan@gmail.com>
2013-11-14 00:58:09 -0500

    adding setup.cfg to make RPMs easier

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from distutils.core import setup, Extension
 
 setup(name='NewRHELic',
     version='0.1',
-    release='2',
     description='RHEL/CentOS monitoring plugin for New Relic',
     author='Jamie Duncan',
     author_email='jduncan@redhat.com',
     url='https://github.com/jduncan-rva/newRHELic',
+    platform=['Linux'],
+    maintainer='Jamie Duncan',
+    maintainer_email = 'jduncan@redhat.com',
+    long_description='A RHEL 6/CentOS 6-specific monitoring plugin for New Relic',
     packages=['psutil','daemon','daemon.version','lockfile'],
     py_modules=['newrelic'],
     ext_modules=[
