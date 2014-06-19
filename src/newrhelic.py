342606da8aaa4fdf658a5bd31723d81c0bfb7c7c
Tommy McNeely <tmcneely@deliveryagent.com>
2014-06-18 21:17:11 -0700

    Fix for Issue #17 - moving data out of config
    * Moved the following from the config file to the __init__
    ** api_url
    ** guid
    ** name
    ** version
    * Renamed duration -> interval (since I think it makes more sense
    * Added pidfile to config
    * Updated "script" to use pidfile and duration
    * bumped version from '0.1' to '0.2.0' (symatic versioning)

--- src/newrhelic.py
+++ src/newrhelic.py
@@ -37,6 +37,10 @@ class NewRHELic:
 
     def __init__(self, debug=False, conf='/etc/newrhelic.conf'):
 
+        self.guid = 'com.rhel.os_statistics'
+        self.name = 'OS Statistics'
+        self.version = '0.2.0'
+        self.api_url = 'https://platform-api.newrelic.com/platform/v1/metrics'
         self.config_file = conf
         socket.setdefaulttimeout(5)
 
@@ -103,11 +107,8 @@ class NewRHELic:
 
         try:
             self.license_key = config.get('site', 'key')
-            self.api_url = config.get('site', 'url')
-            self.duration = config.getint('plugin', 'duration')
-            self.guid = config.get('plugin', 'guid')
-            self.name = config.get('plugin', 'name')
-            self.version = config.get('plugin','version')
+            self.pid_file = config.get('plugin', 'pidfile')
+            self.interval = config.getint('plugin', 'interval')
             self.enable_proxy = config.getboolean('proxy','enable_proxy')
 
             if self.enable_proxy:
@@ -392,7 +393,7 @@ class NewRHELic:
             c_dict = {}
             c_dict['name'] = self.hostname
             c_dict['guid'] = self.guid
-            c_dict['duration'] = self.duration
+            c_dict['duration'] = self.interval
 
             #always get the sys information
             self._get_sys_info()
